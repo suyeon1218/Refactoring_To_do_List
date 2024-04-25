91cd5bed1c60616fdb22eca9ad4ad1cdf2728583
Suyeon <msjgr2801@gmail.com>
2024-04-25 15:51:55 +0900

    feat: 초기 코드

---
+++ index.js
@@ -0,0 +1,59 @@
+const to_do_form = document.querySelector('#to-do-form');
+const to_do_input = document.querySelector('#to-do-input');
+const to_do_submit = document.querySelector('#to-do-submit');
+const ul = document.querySelector('#lists');
+const TODOLIST = 'lists';
+let to_do_lists = JSON.parse(localStorage.getItem(TODOLIST));
+
+function remove_list(event) {
+	const list_value = event.target.previousSibling.data;
+	const remove_index = () => {
+		for (let index = 0; index < to_do_lists.length; index++) {
+			if (to_do_lists[index] == list_value) {
+				return index;
+			}
+		}
+	};
+
+	event.preventDefault();
+	event.target.parentElement.remove();
+	to_do_lists.splice(remove_index(), 1);
+	localStorage.setItem(TODOLIST, JSON.stringify(to_do_lists));
+}
+
+function make_lists(todo_value) {
+	const li = document.createElement('li');
+	const button = document.createElement('button');
+
+	li.innerText = todo_value;
+	button.innerText = '❌';
+
+	ul.appendChild(li);
+	li.appendChild(button);
+
+	button.addEventListener('click', remove_list);
+}
+
+function save_lists() {
+	localStorage.setItem(TODOLIST, JSON.stringify(to_do_lists));
+}
+
+function form_submit_event(event) {
+	const list_value = to_do_input.value;
+
+	to_do_lists.push(list_value);
+	to_do_input.value = '';
+	event.preventDefault();
+	make_lists(list_value);
+	save_lists();
+}
+
+if (to_do_lists === null) {
+	to_do_lists = [];
+} else {
+	to_do_lists.forEach((element) => {
+		make_lists(element);
+	});
+}
+
+to_do_form.addEventListener('submit', form_submit_event);
